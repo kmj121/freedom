580a69c81fcce201105514b969685bac976ef039
kanmeijie@17zhiliao.com <kanmeijie@17zhiliao.com>
2021-09-29 10:52:21 +0800

    项目跑起来

--- src/main/java/com/roger/freedom/FreedomApplication.java
+++ src/main/java/com/roger/freedom/FreedomApplication.java
@@ -1,9 +1,11 @@
 package com.roger.freedom;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.roger.freedom.mapper"})
 public class FreedomApplication {
 
     public static void main(String[] args) {
