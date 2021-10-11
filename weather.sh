c287d1ede067a60d4d19d5ff72c8226c2ac6065b
kanmeijie@17zhiliao.com <kanmeijie@17zhiliao.com>
2021-10-11 09:37:48 +0800

    Shenzhen -> Shanghai

--- weather.sh
+++ weather.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux
 
-CITY=Shenzhen
+CITY=Shanghai
 LANGUAGE="zh-CN"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
@@ -10,4 +10,11 @@ curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   -o result.html \
-  wttr.in/$CITY?format=4\&$UNIT
+  wttr.in/$CITY?format=4\&$UNIT\
+
+# 直接在终端中执行
+#curl \
+#  -H "Accept-Language: zh-CN" \
+#  -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36" \
+#  -o result.html \
+#  wttr.in/Shenzhen?format=4\&m
